974d1aa6263821bcc6f69850b56c84355173804e
StephD48 <93282155+StephD48@users.noreply.github.com>
2021-12-20 12:09:25 -0800

    Create Inheritance.py

---
+++ Python files/Inheritance.py
@@ -0,0 +1,25 @@
+
+class Animal:
+    #Parent class
+    #Define the attributes of the class
+    Animal Type = 'Amphibian'
+    Animal Name = 'Red-Eyed Tree Frog'
+    Warm Blooded = 'No'
+    Cold Blooded = 'Yes'
+    Diet = 'Carnivore'
+    Habitat = 'Rainforest'
+    Number of Legs = '4'
+
+class Dog(Animal):
+    #Child class to Animal parent class
+    Color = 'Black'
+    Sound = 'Bark'
+    Energy Level: 'Active'
+
+class Bird(Animal):
+    #Child class to Animal parent class
+    Feathers = 'Yes'
+    Lay eggs = 'Yes'
+    Running Speed = '43 mph'
+
+    
