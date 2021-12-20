9aef09f5b026e3357c36f5ba1c7e516b84623faf
StephD48 <93282155+StephD48@users.noreply.github.com>
2021-12-20 13:10:59 -0800

    Update Inheritance.py

--- Python files/Inheritance.py
+++ Python files/Inheritance.py
@@ -1,34 +1,34 @@
 class Animal:
     #Parent class
     #Define the attributes of the class
-    Animal Type = ' '
-    Animal Name = ' '
-    Warm Blooded = ' '
-    Cold Blooded = ' '
+    Type = ' '
+    Name = ' '
+    Warm_Blooded = ' '
+    Cold_Blooded = ' '
     Diet = ' '
     Habitat = ' '
-    Number of Legs = ' '
+    Legs = ' '
 
 class Animal:
     #Example of input by a user into the Animal Class
-    Animal Type = 'Amphibian'
-    Animal Name = 'Red-Eyed Tree Frog'
-    Warm Blooded = 'No'
-    Cold Blooded = 'Yes'
+    Type = 'Amphibian'
+    Name = 'Red-Eyed Tree Frog'
+    Warm_Blooded = 'No'
+    Cold_Blooded = 'Yes'
     Diet = 'Carnivore'
     Habitat = 'Rainforest'
-    Number of Legs = '4'
+    Legs = '4'
 
 class Dog(Animal):
     #Child class to Animal parent class
     Color = 'Black'
     Sound = 'Bark'
-    Energy Level: 'Active'
+    Energy_Level: 'Active'
 
 class Bird(Animal):
     #Child class to Animal parent class
     Feathers = 'Yes'
-    Lay eggs = 'Yes'
-    Running Speed = '43 mph'
+    Lay_eggs = 'Yes'
+    Speed = '43 mph'
 
     
